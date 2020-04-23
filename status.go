c77611c59b14944a1132e2fe418b97b351d9baf7
Nikita Vanyasin <nikita.vanyasin@gmail.com>
2020-04-23 10:44:16 +0300

    Update statuses list

--- status.go
+++ status.go
@@ -1,14 +1,24 @@
 package tinkoff
 
 const (
-	StatusNew             = "NEW"              // Платеж создан в системе банка
-	StatusAuthorized      = "AUTHORIZED"       // Деньги захолдированы на карте клиента. Ожидается подтверждение операции
-	StatusConfirmed       = "CONFIRMED"        // Операция подтверждена
-	StatusReversed        = "REVERSED"         // Операция отменена
-	StatusRefunded        = "REFUNDED"         // Произведён возврат
-	StatusPartialRefunded = "PARTIAL_REFUNDED" // Произведён частичный возврат
-	StatusRejected        = "REJECTED"         // Списание денежных средств закончилась ошибкой
-	StatusCanceled        = "CANCELED"         // Неоплаченный платеж отменен
+	StatusNew             = "NEW"              // Создан
+	StatusFormShowed      = "FORMSHOWED"       // Платежная форма открыта покупателем
+	StatusDeadlineExpired = "DEADLINE_EXPIRED" // Просрочен
+	StatusCanceled        = "CANCELED"         // Отменен
+	StatusPreauthorizing  = "PREAUTHORIZING"   // Проверка платежных данных
+	StatusAuthorizing     = "AUTHORIZING"      // Резервируется
+	StatusAuthorized      = "AUTHORIZED"       // Зарезервирован
+	StatusAuthFail        = "AUTH_FAIL"        // Не прошел авторизацию
+	StatusRejected        = "REJECTED"         // Отклонен
+	Status3DSChecking     = "3DS_CHECKING"     // Проверяется по протоколу 3-D Secure
+	Status3DSChecked      = "3DS_CHECKED"      // Проверен по протоколу 3-D Secure
+	StatusReversing       = "REVERSING"        // Резервирование отменяется
+	StatusReversed        = "REVERSED"         // Резервирование отменено
+	StatusConfirming      = "CONFIRMING"       // Подтверждается
+	StatusConfirmed       = "CONFIRMED"        // Подтвержден
+	StatusRefunding       = "REFUNDING"        // Возвращается
+	StatusPartialRefunded = "PARTIAL_REFUNDED" // Возвращен частично
+	StatusRefunded        = "REFUNDED"         // Возвращен полностью
 )
 
 func IsRefundableStatus(status string) bool {
