a6e48c2b6bbc66e8ec62177a8814bd3b170d2c16
Eugene Prigornitskiy <ionixxx@gmail.com>
2024-08-19 04:09:31 +0300

    parse SpAccumulationId for E2C

--- notification.go
+++ notification.go
@@ -9,20 +9,21 @@ import (
 )
 
 type Notification struct {
-	TerminalKey    string            `json:"TerminalKey"` // Идентификатор магазина
-	OrderID        string            `json:"OrderId"`     // Номер заказа в системе Продавца
-	Success        bool              `json:"Success"`     // Успешность операции
-	Status         string            `json:"Status"`      // Статус платежа (см. описание статусов операций)
-	PaymentID      uint64            `json:"PaymentId"`   // Уникальный идентификатор платежа. В случае нотификаций банк присылает число, а не строку, как в случае с Init или Cancel
-	ErrorCode      string            `json:"ErrorCode"`   // Код ошибки, если произошла ошибка
-	Amount         uint64            `json:"Amount"`      // Текущая сумма транзакции в копейках
-	RebillID       string            `json:"RebillId"`    // Идентификатор рекуррентного платежа
-	CardID         uint64            `json:"CardId"`      // Идентификатор привязанной карты
-	PAN            string            `json:"Pan"`         // Маскированный номер карты
-	DataStr        string            `json:"DATA"`
-	Data           map[string]string `json:"-"`       // Дополнительные параметры платежа, переданные при создании заказа
-	Token          string            `json:"Token"`   // Подпись запроса
-	ExpirationDate string            `json:"ExpDate"` // Срок действия карты
+	TerminalKey      string            `json:"TerminalKey"` // Идентификатор магазина
+	OrderID          string            `json:"OrderId"`     // Номер заказа в системе Продавца
+	Success          bool              `json:"Success"`     // Успешность операции
+	Status           string            `json:"Status"`      // Статус платежа (см. описание статусов операций)
+	PaymentID        uint64            `json:"PaymentId"`   // Уникальный идентификатор платежа. В случае нотификаций банк присылает число, а не строку, как в случае с Init или Cancel
+	ErrorCode        string            `json:"ErrorCode"`   // Код ошибки, если произошла ошибка
+	Amount           uint64            `json:"Amount"`      // Текущая сумма транзакции в копейках
+	RebillID         string            `json:"RebillId"`    // Идентификатор рекуррентного платежа
+	CardID           uint64            `json:"CardId"`      // Идентификатор привязанной карты
+	PAN              string            `json:"Pan"`         // Маскированный номер карты
+	DataStr          string            `json:"DATA"`
+	Data             map[string]string `json:"-"`                // Дополнительные параметры платежа, переданные при создании заказа
+	Token            string            `json:"Token"`            // Подпись запроса
+	ExpirationDate   string            `json:"ExpDate"`          // Срок действия карты
+	SpAccumulationID string            `json:"SpAccumulationId"` // Идентификатор накопителя для сделок E2C
 }
 
 func (n *Notification) GetValuesForToken() map[string]string {
@@ -48,6 +49,10 @@ func (n *Notification) GetValuesForToken() map[string]string {
 
 	if n.RebillID != "" {
 		result["RebillId"] = n.RebillID
+	}
+
+	if n.SpAccumulationID != "" {
+		result["SpAccumulationId"] = n.SpAccumulationID
 	}
 
 	return result
