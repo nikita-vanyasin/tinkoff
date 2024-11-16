530792a0fb1defec74ae5a4acecbb3e6e16c9e02
Alexey Milko <alexey.milko@gmail.com>
2024-11-16 14:34:08 +0500

    add check order method

---
+++ checkorder.go
@@ -0,0 +1,46 @@
+package tinkoff
+
+import (
+	"context"
+)
+
+type CheckOrderRequest struct {
+	BaseRequest
+	OrderID string `json:"OrderId"` // Номер заказа в системе Продавца
+}
+
+type CheckOrderResponse struct {
+	BaseResponse
+	OrderID  string `json:"OrderId"` // Номер заказа в системе Продавца
+	Payments []struct {
+		PaymentID    string `json:"PaymentId"`
+		Amount       uint64 `json:"Amount"`            // Стоимость товара в копейках
+		Status       string `json:"Status"`            // Статус платежа
+		RRN          string `json:"RRN"`               // Внутренний номер операции в платежной системе — кроме операций по СБП.
+		Success      bool   `json:"Success"`           // Успешность операции
+		ErrorCode    string `json:"ErrorCode"`         // Код ошибки, «0» - если успешно
+		ErrorMessage string `json:"Message,omitempty"` // Краткое описание ошибки
+	}
+}
+
+func (i *CheckOrderRequest) GetValuesForToken() map[string]string {
+	return map[string]string{
+		"OrderID": i.OrderID,
+	}
+}
+
+func (c *Client) CheckOrderWithContext(ctx context.Context, request *CheckOrderRequest) (*CheckOrderResponse, error) {
+	response, err := c.PostRequestWithContext(ctx, "/CheckOrder", request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	var res CheckOrderResponse
+	err = c.decodeResponse(response, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, res.Error()
+}
