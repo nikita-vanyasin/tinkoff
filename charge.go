11721d543cbe858f376da26750301932f8fce8ce
mazhigali <mazhigali@gmail.com>
2024-12-12 18:13:55 +0300

    add charge

---
+++ charge.go
@@ -0,0 +1,62 @@
+package tinkoff
+
+import (
+	"context"
+	"fmt"
+)
+
+type ChargeRequest struct {
+	BaseRequest
+
+	PaymentId string `json:"PaymentId,omitempty"` // Уникальный идентификатор транзакции в системе Т‑Кассы
+	RebillId  string `json:"RebillId,omitempty"`  // Идентификатор рекуррентного платежа
+	IP        string `json:"IP,omitempty"`        // IP-адрес клиента
+	SendEmail bool   `json:"SendEmail,omitempty"` // true — если клиент хочет получать уведомления на почту.
+	InfoEmail string `json:"InfoEmail,omitempty"` // Адрес почты клиента. Обязателен при передаче SendEmail
+}
+
+func (i *ChargeRequest) GetValuesForToken() map[string]string {
+	v := map[string]string{
+		"PaymentId": i.PaymentId,
+		"RebillId":  i.RebillId,
+		"IP":        i.IP,
+		"SendEmail": serializeBool(i.SendEmail),
+		"InfoEmail": i.InfoEmail,
+	}
+	return v
+}
+
+type ChargeResponse struct {
+	BaseResponse
+	Amount    uint64 `json:"Amount"`    // Сумма в копейках
+	OrderID   string `json:"OrderId"`   // Номер заказа в системе Продавца
+	Status    string `json:"Status"`    // Статус транзакции
+	PaymentID string `json:"PaymentId"` // Уникальный идентификатор транзакции в системе Банка. По офф. документации это number(20), но фактически значение передается в виде строки.
+}
+
+// Charge проводит рекуррентный (повторный) платеж
+func (c *Client) Charge(request *ChargeRequest) (*ChargeResponse, error) {
+	return c.ChargeWithContext(context.Background(), request)
+}
+
+// ChargeWithContext проводит рекуррентный (повторный) платеж
+func (c *Client) ChargeWithContext(ctx context.Context, request *ChargeRequest) (*ChargeResponse, error) {
+	response, err := c.PostRequestWithContext(ctx, "/Charge", request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	var res ChargeResponse
+	err = c.decodeResponse(response, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	err = res.Error()
+	if res.Status != StatusNew {
+		err = errorConcat(err, fmt.Errorf("unexpected payment status: %s", res.Status))
+	}
+
+	return &res, err
+}
